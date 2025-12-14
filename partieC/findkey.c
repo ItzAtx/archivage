76902abef366ed556a01d2f3e51d67a0e992bf16
Vauchel Anthony <152270813+ItzAtx@users.noreply.github.com>
2025-12-14 23:04:20 +0100

    Add files via upload

---
+++ partieC/findkey.c
@@ -0,0 +1,25 @@
+#include "chiffrement.h"
+
+int main(int argc, char *argv[]) {
+    if (argc == 5) {
+        if (strcmp(argv[3], "-o") == 0){
+            string plain_file = argv[1];
+            string ciphered_file = argv[2];
+            string path = argv[4];
+            findkey(plain_file, ciphered_file, path, 1);
+        } else {
+            printf("Usage : %s <fichier propre> <fichier chiffré> (facultatif : -o <chemin>)\n", argv[0]);
+            exit(EXIT_FAILURE);
+        }
+    } else if (argc == 3){
+        string plain_file = argv[1];
+        string ciphered_file = argv[2];
+        findkey(plain_file, ciphered_file, "", 0);
+    } else {
+        printf("Usage : %s <fichier propre> <fichier chiffré> (faculatatif : -o <chemin>)\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    
+    base64_cleanup();
+}
